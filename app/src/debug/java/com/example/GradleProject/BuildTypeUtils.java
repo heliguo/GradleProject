2f3dc7a8169d7efc79f994b4a192640bc387c341
lgh <ysulgh@163.com>
2021-02-19 15:59:14 +0800

    gradle

---
+++ app/src/debug/java/com/example/GradleProject/BuildTypeUtils.java
@@ -0,0 +1,20 @@
+package com.example.GradleProject;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author lgh on 2021/2/19 10:22
+ * @description
+ */
+public class BuildTypeUtils {
+
+    public static void drawBadge(Activity activity) {
+        ViewGroup decorView = (ViewGroup) activity.getWindow().getDecorView();
+        View badge = new View(activity);
+        badge.setBackgroundColor(Color.RED);
+        decorView.addView(badge,100,100);
+    }
+}
---
+++ app/src/release/java/com/example/GradleProject/BuildTypeUtils.java
@@ -0,0 +1,16 @@
+package com.example.GradleProject;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author lgh on 2021/2/19 10:22
+ * @description
+ */
+public class BuildTypeUtils {
+
+    public static void drawBadge(Activity activity) {
+    }
+}
